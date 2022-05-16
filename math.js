a17f903c0ff7db7246c2da041eab424bcf8e9351
bkcp <bkcp83@gmail.com>
2022-05-16 06:34:17 -0700

    using the require keyword to import another js file. Inside the file we are importing, we have to specify what part of the file we are exporting

---
+++ math.js
@@ -0,0 +1,16 @@
+const add = (x,y) => x + y;
+
+const PI = 3.14;
+const square = x => x*x;
+
+const math = {
+    add: add,
+    PI: PI,
+    square: square
+}
+
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
+
+module.exports =  math;
