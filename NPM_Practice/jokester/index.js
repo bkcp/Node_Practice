af9d8a737849b9d388861f82d1387587eb3b37f0
bkcp <bkcp83@gmail.com>
2022-05-18 06:52:44 -0700

    using NPM to install packages

---
+++ NPM_Practice/jokester/index.js
@@ -0,0 +1,6 @@
+const jokes = require('give-me-a-joke');
+jokes.getRandomDadJoke(function(joke){
+    console.log(joke);
+});
+const colors = require('colors');
+console.log("OMG a rainbow".rainbow);
