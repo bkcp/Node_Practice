e54e77f0569afc3cb5bfaaa50b23b21ec12194f8
bkcp <bkcp83@gmail.com>
2022-05-18 06:24:57 -0700

    requiring files, just use path name. when requiring directories: inside directory have index.js file that exports all the modules. but call require on directory name, not just index.

---
+++ Shelter/index.js
@@ -0,0 +1,6 @@
+const blue = require('./blue');
+const jonsi = require('./jonsi');
+const sadie = require ('./sadie');
+
+const allCats = [blue, jonsi, sadie];
+module.exports = allCats;
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const math = require('./math');
 console.log(math);
 console.log(math.square(9));
+
+const cats = require('./Shelter');
+console.log("Directory required:", cats);
